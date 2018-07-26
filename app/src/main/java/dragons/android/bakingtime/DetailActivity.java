5d0c9a3d491dc648017ff9b33195093e02c01a42
jgebbeken <josh.gebbeken@asu.edu>
2018-07-25 19:43:18 -0700

    Optimized fragment code to be more modular and to work in two pane mode.

--- app/src/main/java/dragons/android/bakingtime/DetailActivity.java
+++ app/src/main/java/dragons/android/bakingtime/DetailActivity.java
@@ -1,5 +1,8 @@
 package dragons.android.bakingtime;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
@@ -9,36 +12,56 @@ import android.webkit.MimeTypeMap;
 
 import dragons.android.bakingtime.model.DetailViewModel;
 import dragons.android.bakingtime.model.Recipe;
+import dragons.android.bakingtime.model.Step;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements IngredientsWithSteps.SelectStep{
+
+    private boolean mTwoPane;
+    Recipe recipe;
+    IngredientsWithSteps ingredientsWithSteps = new IngredientsWithSteps();
+    final FragmentManager fragmentManager =  getSupportFragmentManager();
+    DetailViewModel detailViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        Recipe recipe;
-        recipe = getIntent().getParcelableExtra("sentRecipe");
-
         DetailViewModel detailViewModel = ViewModelProviders.of(this)
                 .get(DetailViewModel.class);
-
-        detailViewModel.setRecipeMutableLiveData(recipe);
-
-        IngredientsWithSteps ingredientsWithSteps = new IngredientsWithSteps();
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-
-        fragmentManager.beginTransaction()
-                .add(R.id.ingredients_and_steps_container, ingredientsWithSteps)
-                .addToBackStack(null)
-                .commit();
+        ingredientsWithSteps.setSelectStep(this);
 
 
-        Log.d("DetailActivity: ", recipe.getName());
+        if(findViewById(R.id.tablet_ingredients_steps) != null){
+            mTwoPane = true;
+        } else {
+            mTwoPane = false;
+            fragmentManager.beginTransaction()
+                    .add(R.id.ingredients_and_steps_container, ingredientsWithSteps)
+                    .addToBackStack(null)
+                    .commit();
+        }
 
-        String test = MimeTypeMap.getFileExtensionFromUrl(recipe.getSteps().get(0).getVideoURL());
 
-        Log.d("MimeType test: ", test);
+    }
+
+
+    @Override
+    public void stepSelected() {
+
+        StepsDetailView stepsDetailView = new StepsDetailView();
+        if(mTwoPane){
+
+            fragmentManager.beginTransaction()
+                    .replace(R.id.tablet_step_detail, stepsDetailView)
+                    .commit();
+
+        }else{
+           fragmentManager.beginTransaction()
+                   .replace(R.id.ingredients_and_steps_container, stepsDetailView)
+                   .addToBackStack(null)
+                   .commit();
+        }
 
     }
 }
